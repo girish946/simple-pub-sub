131a170e332d5ba110f21d297e153902090de784
Girish Joshi <girish946@gmail.com>
2023-11-24 21:35:12 +0530

    added env_logger
    Signed-off-by: Girish Joshi <girish946@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use tokio::net::TcpListener;
 
+use log::info;
+
 use std::env;
 use std::error::Error;
-
+pub const LOG_LEVEL: &str = "trace";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    env::set_var("RUST_LOG", LOG_LEVEL);
+    env_logger::init();
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:6480".to_string());
 
     let listener = TcpListener::bind(&addr).await?;
-    println!("Listening on: {}", addr);
+    info!("Listening on: {}", addr);
 
     loop {
         let (socket, addr) = listener.accept().await?;
-        println!("addr is: {addr}");
+        info!("addr is: {addr}");
     }
 }
