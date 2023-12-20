4c54e81ea83f6c13bdadaec8a3fbb903c35c888b
Girish Joshi <girish946@gmail.com>
2023-12-20 22:08:03 +0530

    used clap for argparsing
    Signed-off-by: Girish Joshi <girish946@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,45 @@ use std::env;
 use std::error::Error;
 use tokio::net::TcpListener;
 
+use clap::Parser;
+
+/// Simple pub-sub
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// host name
+    #[arg(short, long, default_value = "0.0.0.0")]
+    pub host: String,
+
+    /// port
+    #[arg(short, long, default_value_t = 6480)]
+    pub port: u16,
+
+    /// log_level
+    #[arg(short, long, default_value = "trace")]
+    pub log_level: Option<String>,
+}
+
 pub const LOG_LEVEL: &str = "trace";
 pub const DEFAULT_HOST: &str = "0.0.0.0";
 pub const DEFAULT_PORT: u16 = 6480;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    env::set_var("RUST_LOG", LOG_LEVEL);
-    env_logger::init();
+    let levels: Vec<String> = vec![
+        "trace".to_string(),
+        "info".to_string(),
+        "debug".to_string(),
+        "error".to_string(),
+    ];
 
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| format!("{}:{}", DEFAULT_HOST, DEFAULT_PORT));
-
+    let args = Args::parse();
+    let addr = format!("{}:{}", args.host, args.port);
+    if levels.contains(&args.log_level.clone().unwrap()) {
+        env::set_var("RUST_LOG", args.log_level.clone().unwrap());
+        env_logger::init();
+        info!("using log_level: {}", args.log_level.clone().unwrap());
+    }
     let listener = TcpListener::bind(&addr).await?;
     info!("Listening on: {}", addr);
     info!("getting global broadcaster");
