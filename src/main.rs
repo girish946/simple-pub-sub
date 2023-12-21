97530be231ec3a22d34605b8d56ea34cca07f18e
Girish Joshi <girish946@gmail.com>
2023-12-21 22:33:51 +0530

    updated the argparsing logic
    Signed-off-by: Girish Joshi <girish946@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,123 @@
 pub mod client_handler;
 pub mod message;
 pub mod topics;
+use clap::{Parser, Subcommand};
 use log::info;
 use std::env;
 use std::error::Error;
 use tokio::net::TcpListener;
 
-use clap::Parser;
+/// client mode
+#[derive(clap::ValueEnum, Clone)]
+enum ClientType {
+    Publish,
+    Subscribe,
+    Query,
+}
 
-/// Simple pub-sub
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    /// host name
-    #[arg(short, long, default_value = "0.0.0.0")]
-    pub host: String,
+/// log leve for the client/server
+#[derive(clap::ValueEnum, Clone)]
+enum LogLevel {
+    Teace,
+    Warn,
+    Info,
+    Error,
+    Debug,
+}
 
-    /// port
-    #[arg(short, long, default_value_t = 6480)]
-    pub port: u16,
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
 
-    /// log_level
-    #[arg(short, long, default_value = "trace")]
-    pub log_level: Option<String>,
+    /// log level, default: info
+    #[clap(long, global = true)]
+    log_level: Option<LogLevel>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Server {
+        /// hoststring
+        host: String,
+        /// port
+        port: u16,
+    },
+    Client {
+        /// server
+        server: String,
+        /// server port
+        port: u16,
+        /// client mode
+        client_type: ClientType,
+        /// topic to publish/subscribe
+        topic: String,
+        /// message to be published
+        message: Option<String>,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let levels: Vec<String> = vec![
-        "trace".to_string(),
-        "info".to_string(),
-        "debug".to_string(),
-        "error".to_string(),
-    ];
+    let cli = Cli::parse();
 
-    let args = Args::parse();
-    let addr = format!("{}:{}", args.host, args.port);
-    if levels.contains(&args.log_level.clone().unwrap()) {
-        env::set_var("RUST_LOG", args.log_level.clone().unwrap());
-        env_logger::init();
-        info!("using log_level: {}", args.log_level.clone().unwrap());
+    let log_level: &str = match cli.log_level {
+        Some(LogLevel::Teace) => "trace",
+        Some(LogLevel::Warn) => "warn",
+        Some(LogLevel::Info) => "info",
+        Some(LogLevel::Error) => "error",
+        Some(LogLevel::Debug) => "debug",
+        None => "info",
+    };
+    env::set_var("RUST_LOG", log_level);
+    env_logger::init();
+
+    match &cli.command {
+        Commands::Server { host, port } => {
+            let addr = format!("{}:{}", host, port);
+            let listener = TcpListener::bind(&addr).await?;
+            info!("Listening on: {}", addr);
+            info!("getting global broadcaster");
+
+            let tx = topics::get_global_broadcaster();
+
+            let _topic_handler = tokio::spawn(topics::topic_manager(tx.clone()));
+
+            loop {
+                let (socket, addr) = listener.accept().await?;
+                info!("addr is: {addr}");
+                client_handler::handle_clinet(socket, tx.clone()).await;
+            }
+        }
+        Commands::Client {
+            server,
+            port,
+            client_type,
+            topic,
+            message,
+        } => {
+            let addr = format!("{server}:{port}");
+            info!("connecting to: {addr}");
+            match client_type {
+                ClientType::Publish => {
+                    info!(
+                        "Publishing message '{}' on topic '{}'",
+                        message.as_deref().unwrap_or(""),
+                        topic
+                    );
+                    // Publish logic here
+                }
+                ClientType::Subscribe => {
+                    info!("Subscribing to topic '{}'", topic);
+                    // Subscribe logic here
+                }
+                ClientType::Query => {
+                    info!("Querying topic '{}'", topic);
+                    // Query logic here
+                }
+            }
+        }
     }
-    let listener = TcpListener::bind(&addr).await?;
-    info!("Listening on: {}", addr);
-    info!("getting global broadcaster");
-
-    let tx = topics::get_global_broadcaster();
-
-    let _topic_handler = tokio::spawn(topics::topic_manager(tx.clone()));
-
-    loop {
-        let (socket, addr) = listener.accept().await?;
-        info!("addr is: {addr}");
-        client_handler::handle_clinet(socket, tx.clone()).await;
-    }
+    Ok(())
 }
