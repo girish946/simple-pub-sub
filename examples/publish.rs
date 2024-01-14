7364efadce48a6915230bd1e4c8978ab7bd6ff4b
Girish Joshi <girish946@gmail.com>
2024-01-15 00:37:34 +0530

    added sample to publish a message

---
+++ examples/publish.rs
@@ -0,0 +1,18 @@
+// use simple-pub-sub
+use simple_pub_sub;
+
+#[tokio::main]
+async fn main() -> Result<(), String> {
+    // initialize the client.
+    let mut client = simple_pub_sub::client::Client::new("localhost".to_string(), 6480);
+    // connect the client.
+    let _ = client.connect().await;
+    // subscribe to the given topic.
+    let _ = client
+        .publish(
+            "abc".to_string(),
+            "test message".to_string().into_bytes().to_vec(),
+        )
+        .await;
+    Ok(())
+}
